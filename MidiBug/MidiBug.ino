5bb2f30f699fc4e49ea86b64886584fee736c96c
Øystein Fledsberg <oysteinf@microsoft.com>
2018-03-24 21:57:17 +0100

    Add project files.

---
+++ MidiBug/MidiBug.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MidiBug.ino
+ Created:	3/24/2018 9:57:04 PM
+ Author:	oysteinf
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
